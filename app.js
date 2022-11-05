8414b40f45704e9ff39478204a54fdd100a8bb07
Trace Sweeney <sweenetr@flip1.engr.oregonstate.edu>
2022-11-04 20:43:21 -0700

    added add functionality to all tables

--- app.js
+++ app.js
@@ -67,9 +67,14 @@ app.get('/bookings', function(req, res)                 // This is the basic syn
             ON Bookings.rental_id = Rentals.rental_id \
             WHERE booking_id LIKE "${req.query.booking_id}%";`;
         }
-        db.pool.query(query1, function(error, rows, fields){    // Execute the query
 
-            res.render('bookings', {data: rows});                  // Render the bookings.hbs file, and also send the renderer
+        let query2 = "SELECT * FROM Rentals"
+        db.pool.query(query1, function(error, rows, fields){    // Execute the query
+            let bookings = rows
+            db.pool.query(query2, function(error, rows, fields){    // Execute the query
+                let rentals = rows
+                res.render('bookings', {data: bookings, rentals: rentals});                  // Render the bookings.hbs file, and also send the renderer
+            })                  // Render the bookings.hbs file, and also send the renderer
         })    
     });    
 
@@ -137,7 +142,7 @@ app.post('/add-rental-form', function(req, res){
 
     // Capture NULL values (Does not work correctly) *************
 
-    let rental_name = parseInt(data['input_rental_name']);
+    let rental_name = data['input_rental_name'];
     if (rental_name.length === 0)
     {
         rental_name = "NULL"
@@ -178,7 +183,220 @@ app.post('/add-rental-form', function(req, res){
 })
 
 
-//
+/* Allows adding of new guest */
+app.post('/add-guest-form', function(req, res){
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+    // Capture NULL values (Does not work correctly) *************
+
+    let guest_first_name = data['input_guest_first_name'];
+    if (guest_first_name.length === 0)
+    {
+        guest_first_name = "NULL"
+    }
+
+    let guest_last_name = data['input_guest_last_name'];
+    if (guest_last_name.length === 0)
+    {
+        guest_last_name = "NULL"
+    }
+
+    let guest_email = data['input_guest_email'];
+    if (guest_email.length === 0)
+    {
+        guest_email = "NULL"
+    }
+    
+    let guest_phone = data['input_guest_phone'];
+    if (guest_phone.length === 0)
+    {
+        guest_phone = "NULL"
+    }
+
+    let guest_address = data['input_guest_address'];
+    if (guest_address.length === 0)
+    {
+        guest_address = "NULL"
+    }
+
+
+    // Create the query and run it on the database
+    query1 = `INSERT INTO Guests (first_name, last_name, email, phone, address) VALUES ("${guest_first_name}", "${guest_last_name}", "${guest_email}", "${guest_phone}", "${guest_address}")`;
+    db.pool.query(query1, function(error, rows, fields){
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+
+        // If there was no error, we redirect back to our root route, which automatically runs the SELECT * FROM bsg_people and
+        // presents it on the screen
+        else
+        {
+            res.redirect('guests');
+        }
+    })
+})
+
+
+/* Allows the addition of new rental types */
+app.post('/add-rental-types-form', function(req, res){
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+    // Capture NULL values (Does not work correctly) *************
+
+    let rental_type_name = data['input_rental_type_name'];
+    if (rental_type_name.length === 0)
+    {
+        rental_type_name = "NULL"
+    }
+
+    let availability = data['input_availability'];
+    if (availability.length === 0)
+    {
+        availability = "NULL"
+    }
+
+    let occupancy = parseInt(data['input_occupancy']);
+    if (isNaN(occupancy))
+    {
+        occupancy = 'NULL'
+    }
+    
+    let cost_per_night = parseInt(data['input_cost_per_night']);
+    if (isNaN(cost_per_night))
+    {
+        cost_per_night = 'NULL'
+    }
+
+
+    // Create the query and run it on the database
+    query1 = `INSERT INTO Rental_Types (rental_type_name, availability, occupancy, cost_per_night) VALUES ("${rental_type_name}", "${availability}", ${occupancy}, ${cost_per_night})`;
+    db.pool.query(query1, function(error, rows, fields){
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+
+        // If there was no error, we redirect back to our root route, which automatically runs the SELECT * FROM bsg_people and
+        // presents it on the screen
+        else
+        {
+            res.redirect('rental_types');
+        }
+    })
+})
+
+
+
+/* Allows adding of new employee */
+app.post('/add-employee-form', function(req, res){
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+    // Capture NULL values (Does not work correctly) *************
+
+    let employee_first_name = data['input_employee_first_name'];
+    if (employee_first_name.length === 0)
+    {
+        employee_first_name = "NULL"
+    }
+
+    let employee_last_name = data['input_employee_last_name'];
+    if (employee_last_name.length === 0)
+    {
+        employee_last_name = "NULL"
+    }
+
+    let employee_email = data['input_employee_email'];
+    if (employee_email.length === 0)
+    {
+        employee_email = "NULL"
+    }
+    
+    let employee_phone = data['input_employee_phone'];
+    if (employee_phone.length === 0)
+    {
+        employee_phone = "NULL"
+    }
+
+
+    // Create the query and run it on the database
+    query1 = `INSERT INTO Employees (first_name, last_name, email, phone) VALUES ("${employee_first_name}", "${employee_last_name}", "${employee_email}", "${employee_phone}")`;
+    db.pool.query(query1, function(error, rows, fields){
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+
+        // If there was no error, we redirect back to our root route, which automatically runs the SELECT * FROM bsg_people and
+        // presents it on the screen
+        else
+        {
+            res.redirect('employees');
+        }
+    })
+})
+
+/* Allows the addition of a new booking  --------- Booking Date not working*/
+app.post('/add-booking-form', function(req, res){
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+    // Capture NULL values (Does not work correctly) *************
+
+    let number_of_guests = parseInt(data['input_number_of_guests']);
+    if (isNaN(number_of_guests))
+    {
+        number_of_guests = 'NULL'
+    }
+
+    let rental = parseInt(data['input_rental']);
+    if (isNaN(rental))
+    {
+        rental = 'NULL'
+    }
+
+    let total_cost = parseInt(data['input_total_cost']);
+    if (isNaN(total_cost))
+    {
+        total_cost = 'NULL'
+    }
+
+    // Create the query and run it on the database
+    query1 = `INSERT INTO Bookings (number_of_guests, rental_id, check_in_date, check_out_date, total_cost) VALUES (${number_of_guests}, ${rental}, "${data['check_in_date']}", "${data['check_out_date']}", ${total_cost})`;
+    db.pool.query(query1, function(error, rows, fields){
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+
+        // If there was no error, we redirect back to our root route, which automatically runs the SELECT * FROM bsg_people and
+        // presents it on the screen
+        else
+        {
+            res.redirect('bookings');
+        }
+    })
+})
+
 
 /*
     LISTENER
